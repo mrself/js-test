91503b79cf6a2d531efbe6487e977941f185ec33
Lightman <yaruk.my@gmail.com>
2019-09-15 15:15:33 +0300

    setup order modal window

--- src/js/Components/OrderComponent.vue
+++ src/js/Components/OrderComponent.vue
@@ -1,10 +1,27 @@
 <template>
-    
+    <div>
+        <div class="order" v-show="showModal">
+            <button class="order__close" @click="showModal = false">
+                <i class="fas fa-arrow-left"></i>
+            </button>
+            <div class="order__img">
+                <img :src="order.images.thumbnail" :alt="order.name">
+            </div>
+        </div>
+
+        <button @click="showModal = true">Create new content</button>
+    </div>
 </template>
 
 <script>
     export default {
         name: "OrderComponent",
+
+        data() {
+            return {
+                showModal: false
+            };
+        },
 
         props: {
             order: {
